9d911cf5f8234d9ec218d0a3e689212e373b09c7
karacam <kkaracam96@gmail.com>
2024-11-03 15:50:23 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey there! Seems like you know nothing about fronted development...
         </p>
         <a
           className="App-link"
